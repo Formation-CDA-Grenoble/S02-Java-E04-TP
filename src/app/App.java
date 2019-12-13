400a51ec5f3290d0f2989ae19a0370048e367efe
Romain Tello <romain.tello@gmail.com>
2019-12-13 09:50:41 +0100

    Add test method to App class

--- src/app/App.java
+++ src/app/App.java
@@ -93,4 +93,12 @@ public class App {
         System.out.println("Créons Mario avec 3 vies...");
         return new Character();
     }
+
+    private static void test(String call, Object actualValue, Object expectedValue) {
+        if (isEqual(actualValue, expectedValue)) {
+            System.out.println("L'appel " + call + " renvoie: " + actualValue +". (valeur attendue: " + expectedValue + ") C'est bon! ☑️");
+        } else {
+            System.out.println("L'appel " + call + " renvoie: " + actualValue +". (valeur attendue: " + expectedValue + ") Encore un coup des Illuminati... ❔");
+        }
+    }
 }
